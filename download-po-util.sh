6b1b0a0eb2ab237c41ca34c62d20e1b2a622347f
Nathan Robinson <nrobinson2000@me.com>
2016-04-23 10:31:59 +0100

    Update download-po-util.sh

--- download-po-util.sh
+++ download-po-util.sh
@@ -3,4 +3,4 @@ cd ~
 curl -fsSLO https://raw.githubusercontent.com/nrobinson2000/po-util/master/po-util.sh
 chmod +x po-util.sh
 echo 'alias po="~/po-util.sh"' >> .bashrc
-~/po-util.sh install && echo && echo "Sucessfully installed the Particle Offline Utility and necessary dependencies!" && echo "Read more at https://raw.githubusercontent.com/nrobinson2000/po-util/"
+~/po-util.sh install && echo && echo "Sucessfully installed the Particle Offline Utility and necessary dependencies!" && echo "Read more at https://github.com/nrobinson2000/po-util"
