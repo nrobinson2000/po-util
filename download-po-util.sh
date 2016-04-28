96a0b72e8d612b6a0d372b78d87b3186ee00f81c
Nathan Robinson <nrobinson2000@me.com>
2016-04-28 15:25:07 +0100

    at 15:25 of 28-04-2016

--- download-po-util.sh
+++ download-po-util.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
-curl -fsSLO https://raw.githubusercontent.com/nrobinson2000/po-util/master/po-util.sh
-cp po-util.sh ~/po-util.sh
+if [ -f ~/po-util.sh ];
+then
+  rm ~/po-util.sh
+  curl -fsSLO https://raw.githubusercontent.com/nrobinson2000/po-util/master/po-util.sh
+  mv po-util.sh ~/po-util.sh
+  chmod +x ~/po-util.sh
+else
+  curl -fsSLO https://raw.githubusercontent.com/nrobinson2000/po-util/master/po-util.sh
+  mv po-util.sh ~/po-util.sh
+fi
+
+if [ -f ~/.bash_profile ];
+then
+  echo
+else
+echo "
+export PATH=\"/usr/local/sbin:$PATH\"
+if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+fi" >> ~/.bash_profile
+fi
+
+if [ -f ~/.bashrc ];
+then
+  echo ".bashrc present"
+  if cat ~/.bashrc | grep "po-util.sh" ;
+  then
+    echo "po alias already in place."
+  else
+    echo "no po alias."
+    echo 'alias po="~/po-util.sh"' >> ~/.bashrc
+  fi
+else
+  echo "No .bashrc present"
+  echo 'alias po="~/po-util.sh"' >> ~/.bashrc
+fi
 
 ~/po-util.sh install && echo && echo "Sucessfully installed the Particle Offline Utility and necessary dependencies!" && echo "Read more at https://github.com/nrobinson2000/po-util"
