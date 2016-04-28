397ae2a4948891a87c670e6a4d043d2de32d87a4
Nathan Robinson <nrobinson2000@me.com>
2016-04-28 13:59:50 +0100

    Updated "po help" documentation, download script, and ci files.

--- download-po-util.sh
+++ download-po-util.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-cd ~
-curl -fsSLO https://raw.githubusercontent.com/nrobinson2000/po-util/master/po-util.sh
-chmod +x po-util.sh
-echo 'alias po="~/po-util.sh"' >> .bashrc
+if [ -f ~/po-util.sh ];
+then
+  rm ~/po-util.sh
+  cp po-util.sh ~/po-util.sh
+else
+  cp po-util.sh ~/po-util.sh
+fi
+
+if [ -f ~/.bashrc ];
+then
+  echo ".bashrc present"
+  if cat ~/.bashrc | grep "po-util.sh" ;
+  then
+    echo "po alias already in place."
+  else
+    echo "no po alias."
+    echo 'alias po="~/po-util.sh"' >> ~/.bashrc
+  fi
+else
+  echo "No .bashrc present"
+  echo 'alias po="~/po-util.sh"' >> ~/.bashrc
+fi
+
+if [ "$1" == "install" ];
+then
 ~/po-util.sh install && echo && echo "Sucessfully installed the Particle Offline Utility and necessary dependencies!" && echo "Read more at https://github.com/nrobinson2000/po-util"
+fi
--- po-util.sh
+++ po-util.sh
@@ -4,50 +4,54 @@
 
 if [ "$1" == "help" ];
 then
-echo "po-util 1.2
+echo "po-util 1.3 Copyright (C) 2016  Nathan Robinson
+This program comes with ABSOLUTELY NO WARRANTY.
 
-Usage:
+== Usage ==
 
 Put your device into dfu mode and install the firmware patch with:
-po DEVICE patch
--- replace DEVICE with either \"photon\" or \"electron\"
+  \"po DEVICE patch\"
+
+-- replace DEVICE with either \"photon\" or \"electron\" --
 
 To format your working directory into a project folder, run:
-po init
+  \"po init\"
 
 To compile and test your firmware, run:
-po DEVICE build
+  \"po DEVICE build\"
 
 To compile and automagically upload your firmware with dfu-util, run:
-po DEVICE flash
+  \"po DEVICE flash\"
 
 To reset the build directory, run:
-po DEVICE clean
+  \"po DEVICE clean\"
 
 To download new firmware from Particle, run:
-po DEVICE update
+  \"po DEVICE update\"
 
-To upgrade the system firmware on your device to the latest Particle release, run:
-po DEVICE upgrade
+To upgrade firmware on your device to the latest Particle release, run:
+  \"po DEVICE upgrade\"
 
 To instantly flash code that you just compiled with \"build\", run:
-po dfu
+  \"po dfu\"
 
 To put your device into DFU mode, run:
-po dfu-open
+  \"po dfu-open\"
 
 To make your device exit DFU mode, run:
-po dfu-close
+  \"po dfu-close\"
 
-If you want to upload code that you just compiled with \"build\" Over The Air, run:
-po DEVICE ota DEVICE_NAME
- -- replace DEVICE_NAME with the name of your device" | less
+If you want to upload code compiled with \"build\" using Over The Air, run:
+  \"po DEVICE ota DEVICE_NAME\"
+
+ -- replace DEVICE_NAME with the name of your device --
+ " | less
   exit
 fi
 
 if [ "$(uname -s)" == "Darwin" ];
 then
-modem=`ls -1 /dev/cu.* | grep -vi bluetooth | tail -1`
+modem="$(ls -1 /dev/cu.* | grep -vi bluetooth | tail -1)"
 fi
 
 CWD=$(pwd)
@@ -117,14 +121,14 @@ fi
 if [ -d "firmware" ];
 then echo
 else echo "Please run with \"init\" to setup this repository.
-Run \"po help\" for information about this tool." && exit
+For details type \"po help\"" && exit
 fi
 
 if [ "$1" == "dfu" ];
 then
 if [ "$(uname -s)" == "Darwin" ];
 then
-stty -f $modem 19200
+stty -f "$modem" 19200
 sleep 1
 dfu-util -d 2b04:d006 -a 0 -i 0 -s 0x080A0000:leave -D "$CWD/bin/firmware.bin"
 exit
@@ -140,7 +144,7 @@ if [ "$1" == "dfu-open" ];
 then
 if [ "$(uname -s)" == "Darwin" ];
 then
-stty -f $modem 19200
+stty -f "$modem" 19200
 exit
 else
 stty -F /dev/ttyACM0 19200
@@ -210,7 +214,7 @@ if [ "$2" == "flash" ];
 then
 if [ "$(uname -s)" == "Darwin" ];
 then
-stty -f $modem 19200
+stty -f "$modem" 19200
 make all -s -C ~/github/firmware APPDIR="$CWD/firmware" TARGET_DIR="$CWD/bin" PLATFORM="$1" || exit
 dfu-util -d 2b04:d006 -a 0 -i 0 -s 0x080A0000:leave -D "$CWD/bin/firmware.bin"
 
