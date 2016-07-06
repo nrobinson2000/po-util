47e7773ee02a2b26060cbb29c7ad00972c3e8577
Nathan Robinson <nrobinson2000@me.com>
2016-07-06 00:02:30 -0400

    at 00:02 of 06-07-2016

--- donate/index.js
+++ donate/index.js
@@ -5,7 +5,7 @@ var popup = false; // Set to true if you want a popup to pay bitcoin
 var currency_code = "USD"; // Change to your default currency. Choose from https://api.bitcoinaverage.com/ticker/
 var qrcode = true; // Set to false to disable qrcode
 var link = true; // Set to false to disable generating hyperlink
-var organization = "Nathan Robinson"; // Change to your organization name
+var organization = "po-util"; // Change to your organization name
 var mbits = true; // Set to false to display bitcoin traditionally
 
 
