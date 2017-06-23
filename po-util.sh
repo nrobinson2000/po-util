311a0dbe565afc0094d4d00f5caf3c87aa6a6288
Nathan Robinson <nrobinson2000@me.com>
2017-06-23 11:25:39 -0400

    at 11:25 of 2017-06-23

--- po-util.sh
+++ po-util.sh
@@ -718,13 +718,7 @@ cache:
   - $HOME/bin" > "$FIRMWAREDIR/../.travis.yml"
 
   echo "#!/bin/bash
-sudo apt install -y expect
-curl -sLO https://raw.githubusercontent.com/nrobinson2000/po-util/master/po-util.sh
-chmod +x po-util.sh
-curl -sLO https://raw.githubusercontent.com/nrobinson2000/po-util/master/config-expect.sh
-chmod +x config-expect.sh
-./config-expect.sh
-./po-util.sh install ~/github basic
+bash <(curl -sL https://master.po-util.com/ci-install)
 po lib clean . -f &> /dev/null
 yes \"no\" | po lib setup # change to \"yes\" to prefer libraries from GitHub
 po $DEVICE_TYPE build" > "$FIRMWAREDIR/../ci/travis.sh"
