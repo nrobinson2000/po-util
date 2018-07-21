77ac510905afa2d0ea4a5b728b863187198fff46
Nathan Robinson <nrobinson2000@me.com>
2018-07-21 14:33:36 -0400

    [skip ci] at 14:33 of 2018-07-21

---
+++ build-test/bin/obj/src/module_info.o.d
@@ -0,0 +1,33 @@
+/Users/nrobinson/po-util/build-test/bin//obj/src/module_info.o: \
+ src/module_info.c ../../../dynalib/inc/module_info.inc \
+ ../../../dynalib/inc/module_info.h ../../../services/inc/static_assert.h \
+ /usr/local/Cellar/gcc-arm-none-eabi-53/20160330/lib/gcc/arm-none-eabi/5.3.1/include/stddef.h \
+ /usr/local/Cellar/gcc-arm-none-eabi-53/20160330/lib/gcc/arm-none-eabi/5.3.1/include/stdint.h \
+ /usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/stdint.h \
+ /usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/sys/features.h \
+ /usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/sys/_stdint.h
+
+../../../dynalib/inc/module_info.inc:
+
+../../../dynalib/inc/module_info.h:
+
+../../../services/inc/static_assert.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi-53/20160330/lib/gcc/arm-none-eabi/5.3.1/include/stddef.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi-53/20160330/lib/gcc/arm-none-eabi/5.3.1/include/stdint.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/stdint.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/sys/features.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi-53/20160330/arm-none-eabi/include/sys/_stdint.h:
